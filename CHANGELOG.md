920afa51f324b6d4f3382d7cfc180198d6ff1806
Andrew Martin <andrew.thaddeus@gmail.com>
2019-04-16 08:40:14 -0400

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to the [Haskell Package Versioning Policy](https://pvp.haskell.org/).
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ test/Unit.hs
@@ -0,0 +1,21 @@
+import Data.Primitive.Unlifted.TVar
+import Data.Primitive
+import Control.Monad.STM
+
+main :: IO ()
+main = do
+  putStrLn "Start"
+  putStrLn "A"
+  testA
+  putStrLn "Finished"
+
+testA :: IO ()
+testA = do
+  tv <- newUnliftedTVarIO =<< newByteArray 16
+  marr <- newByteArray 8
+  r <- atomically $ do
+    writeUnliftedTVar tv marr
+    readUnliftedTVar tv
+  if sameMutableByteArray marr r
+    then pure ()
+    else fail ""
